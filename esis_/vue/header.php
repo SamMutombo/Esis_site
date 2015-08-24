2c63daea2ae3665e43b8e8d0509a44d111d86c21
Sam <sammutombo4@gmail.com>
2015-08-24 19:04:17 +0200

    Site ESIS en développement
    First commit

---
+++ esis_/vue/header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<!--Alpha team
+*Sam_Lint4, Mahi2, Asky, King_Dan
+-->
+<html>
+<head>
+	<title>Accueil</title>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="utf-8">
+</head>
+<body>
+	<div id="wrapper">
+			<nav>
+				<ul id="menu">
+					<li><a href="#">Valve</a></li>
+					<li><a href="#">Etudiants</a>
+						<ul>
+							<li><a href="#">Les anciens</a></li>
+							<li><a href="#">Les admissions</a></li>
+							<li><a href="#">Le média</a></li>
+						</ul>
+					</li>
+					<li><a href="#">ESIS</a>
+						<ul>
+							<li><a href="#">Le staff</a></li>
+							<li><a href="#">La junior entreprise</a></li>
+							<li><a href="#">Les centres</a></li>
+						</ul>
+					</li>
+					<li><a href="#">Filières</a></li>
+					<li><a href="#">Résultats</a></li>
+				</ul>
+				<form id="formulaire">
+						<input type="text" id="search" name="search" value="" title="Recherche" placeholder="        Recherche...">
+				</form>
+			</nav>
+			<div id="content">
+				<div id="utility">
+					<ul>
+						<li><a href="#">Valve</a></li>
+						<li><a href="#">Etudiants</a></li>
+						<li><a href="#">Anciens</a></li>
+						<li><a href="#">Nos clubs</a></li>
+						<li><a href="#">Média</a></li>
+						<li><a href="#">Contacts</a></li>
+						<li><a href="#">Partenaires</a></li>
+						<li><a href="#">Login</a></li>
+					</ul>
+				</div>
+				<div id="ban">
+					<a href="index.php"><img src="img/logo.png"></a>
+					<h4>E.S.I.S. Lubumbashi</h4>
+					<p><span style="color : green">E</span>cole Supérieure d'Informatique SALAMA</p>
+				</div>
---
+++ esis_/vue/index.php
@@ -0,0 +1,83 @@
+<?php include('header.php') ?>
+				<div id="slide">
+					<img src="img/try.jpg">
+				</div>
+				<!-- Filières   -->
+				<div id="filiere">
+					<div class="lien"> 
+						<a href="#">
+							<figure>
+								<img src="img/i.jpg" alt="La photo de Sam">
+								<figcaption>
+									<center><h1 style='color:#00FF00'>Réseaux</h1></center><br>
+									<p>	
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+										consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+										cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+										proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+									</p>
+								</figcaption>
+							</figure>
+						</a>
+					</div>
+					<div class="lien">
+						<a href="#">
+							<figure>
+								 <div class="rotation">
+									<img src="img/i1.jpg" alt="La photo de Sam" >
+								 </div>
+								
+								<figcaption>
+									<center><h1 style='color:#FFFF00'>Génie Logiciel</h1></center><br>
+									<p>	
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									</p>						
+								</figcaption>
+							</figure>
+						</a>
+					</div>	
+					<div class="lien">	
+						<a href="#">
+							<figure>
+								<img src="img/i2.jpg" alt="La photo de Sam">
+								<figcaption>
+									<center><h1 style='color:#00FFFF'>Design</h1></center><br>
+									<p>	
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									</p>
+								</figcaption>
+							</figure>
+						</a>
+					</div>	
+					<div class="lien">	
+						<a href="#">
+							<figure>
+								<img src="img/i3.jpg" alt="La photo de Sam">
+								<figcaption>
+									<center><h1 style='color:#FF69B4'>Informatique</h1></center><br>
+									<p>	
+										Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+										tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+									</p>
+								</figcaption>
+							</figure>
+						</a>
+					</div>	
+				</div>
+			</div>
+	</div>
+</body>
+<!--Alpha team
+*Sam_Lint4, Mahi2, Asky, King_Dan
+-->
+</html>
